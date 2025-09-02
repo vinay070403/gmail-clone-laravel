ddb5e29ec23eb46496a0ed080f399c48a9ffa8f7
vinay chavada <tanish.chavada@gmail.com>
2025-09-02 17:20:01 +0530

    inbox fot

--- app/Http/Controllers/InboxController.php
+++ app/Http/Controllers/InboxController.php
@@ -10,11 +10,12 @@ class InboxController extends Controller
     public function index()
     {
         $mails = Mail::where('receiver_id', Auth::id())
-            ->where('is_deleted', 0)
+            ->where('is_deleted', 1)
             ->orderBy('created_at', 'desc')
             ->get();
+
+        dump($mails);
 
         return view('inbox', compact('mails'));
     }
 }
-
